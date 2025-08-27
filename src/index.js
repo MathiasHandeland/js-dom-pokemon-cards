12f3221f1028e9f5ff3759483d3fe6df23bdc15b
Mathias Handeland <127216029+MathiasHandeland@users.noreply.github.com>
2025-08-27 11:31:00 +0200

    core finished

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 
-console.log(data);
+const cardsContainer = document.querySelector(".cards"); // all cards go in here
 
-//You can start simple and just render a single 
-//pokemon card from the first element
-console.log(data[0]);
+function drawCards() { // create a card for each pokemon
+    data.forEach(pokemon => {
+       const card = document.createElement("li"); // each card is added to the container as a list item
+       card.className = "card"; // assign the css class to the list item
+       
+        // create the title
+        const title = document.createElement("h2");
+        title.className = "card--title"; // assign the css class to the title
+        title.textContent = pokemon.name;
+        card.appendChild(title);
+
+        // create the image
+        const img = document.createElement("img");
+        img.className = "card--img";
+        img.src = pokemon.sprites.other["official-artwork"].front_default;
+        card.appendChild(img);
+
+        // create the stats list
+        const statsList = document.createElement("ul");
+        statsList.className = "card--text";
+        pokemon.stats.forEach(stat => {
+            const listItem = document.createElement("li");
+            listItem.textContent = `${stat.stat.name.toUpperCase()}: ${stat.base_stat}`;
+            statsList.appendChild(listItem);
+        });
+        card.appendChild(statsList);
+
+        // append the card to the container
+        cardsContainer.appendChild(card);
+    });
+}
+
+drawCards();
